c14676bec9f78a006d2c3bd9ff69ea7524040d61
yz-apr <zhouyi0426@hotmail.com>
2025-08-10 13:41:27 +0900

    update

---
+++ make-r-open-in-project.R
@@ -0,0 +1,59 @@
+##******************************************************************************
+##
+## make R package
+## Open in the project
+##
+##******************************************************************************
+
+library(devtools)
+library(roxygen2)
+library(usethis)
+library(pkgdown)
+
+# pkg <- as.package("C:/Users/zhouy/Documents/GitHub-Phsoft/ICHe9r1/")
+
+# use_gpl_license(version = 3, include_future = TRUE)
+
+## main: load pkg document and check pkg
+document()
+load_all()
+check(document = F, vignettes = F)
+
+tools::checkRd(dir = "man/")
+system("R CMD Rd2pdf C:\\Users\\zhouy\\Documents\\GitHub-Phsoft\\ICHe9r1\\")
+system("R CMD check")
+
+# Sys.setenv(TZ = "UTC")
+
+## create documents --> need manual revision
+# use_readme_rmd()
+# use_news_md()
+
+
+
+## load data --> /data
+dat <- load("data/bmt.rda")
+use_data(bmt, overwrite = TRUE)
+# use_package("survival")
+# use_package("MASS", type = "Depends")
+# use_package("cmprsk", type = "Depends")
+
+
+## build github website --> /docs
+build_readme()
+build_site()
+build_news()
+# use_build_ignore("docs")
+
+
+## build vignette --> /doc
+# use_vignette("ICHer91")
+build_vignettes()
+
+
+## check and update environment
+## https://rstudio.github.io/renv/articles/renv.html
+# library(renv)
+# renv::update()
+# renv::snapshot()
+
--- make.R
+++ make.R
@@ -9,22 +9,26 @@ library(usethis)
 use_readme_rmd()
 usethis::use_news_md()
 
-CD64 <- read.csv("../../GitHub/dtametasa-r/example-results/data-CD64.csv")
-IVD <- read.csv("../../GitHub/dtametasa-r/example-results/data-IVD.csv")
+
+## load data
+dat <- load("data/dat.RData")
+use_data(dat, overwrite = TRUE)
+
+# IVD <- read.csv("../../GitHub/dtametasa-r/example-results/data-IVD.csv")
 
 # Scheidler <- Scheidler[,-1]
-use_data(IVD, overwrite = TRUE)
-use_data(Scheidler, overwrite = TRUE)
+use_data(dat, overwrite = TRUE)
+# use_data(Scheidler, overwrite = TRUE)
 # source("R/data.pre.R")
-IVD_2 <- .logit.data(.correction(IVD))
-Scheidler2 <- logit.data(correction(Scheidler))
-use_data(IVD_2, overwrite = TRUE)
-use_data(Scheidler2, overwrite = TRUE)
+# IVD_2 <- .logit.data(.correction(IVD))
+# Scheidler2 <- logit.data(correction(Scheidler))
+# use_data(IVD_2, overwrite = TRUE)
+# use_data(Scheidler2, overwrite = TRUE)
 
-use_data(CD64)
-CD64_2 <- .logit.data(.correction(CD64))
-use_data(CD64_2)
-use_package("mvtnorm")
+# use_data(CD64)
+# CD64_2 <- .logit.data(.correction(CD64))
+# use_data(CD64_2)
+use_package("survival")
 
 
 
@@ -41,26 +45,28 @@ use_package("mvtnorm")
 
 library(devtools)
 library(roxygen2)
+library(usethis)
 #library(mathjaxr)
-#vignette_roclet()
-#devtools::document()
+# vignette_roclet()
+# devtools::document()
 
 
-pkg <- as.package("../dtametasa/")
-pkg <- as.package("../net.meta/")
+# pkg <- as.package("C:/Users/zhouy/Documents/Gitee-PKU/CSTE/CSTE-main/CSTE-main/")
+pkg <- as.package("C:/Users/zhouy/Documents/GitHub-Phsoft/ICHe9r1/")
 
 ##***********
 document(pkg)
 load_all(pkg)
-check(document = FALSE, vignettes = FALSE)
+check(pkg, document = FALSE, vignettes = FALSE)
 ##***********
 
-devtools::build_readme()
+devtools::build_readme(pkg)
 
 pkgdown::build_site()
 
 pkgdown::build_news()
 
+usethis::use_vignette("ICHer91")
 
 
 ##******************************************************************************
@@ -71,7 +77,7 @@ pkgdown::build_news()
 
 library(devtools)
 library(roxygen2)
-pkg <- as.package("../net.meta/")
+pkg <- as.package("../ICHe9r1/")
 
 load_all(pkg)
 document(pkg)
@@ -79,6 +85,12 @@ load_all()
 check(document = FALSE, vignettes = FALSE)
 
 library(pkgdown)
+
+
 build_site()
 
 update(dev_package_deps(dependencies=TRUE))
+
+
+
+renv::init()
